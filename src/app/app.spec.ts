fbd16163ad9dee3cd8094e417b7e7a2e9fe24597
Tharindu <tharindujayawardhana1@gmail.com>
2026-09-23 23:04:54 +0530

    set up test file

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Forecast.Web');
+    expect(compiled.querySelector('h1')?.textContent).toContain('I-Wonder Forecast Dashboard');
   });
 });
