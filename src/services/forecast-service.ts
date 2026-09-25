8acf924536b18bef9459e0da78b79b48c92e6371
Tharindu <tharindujayawardhana1@gmail.com>
2026-09-25 13:37:45 +0530

    set up backend url

--- src/services/forecast-service.ts
+++ src/services/forecast-service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
 import { Forecast } from '../models/forecast';
 import { Observable } from 'rxjs';
@@ -6,24 +6,36 @@ import { Observable } from 'rxjs';
 @Service()
 export class ForecastService {
     private http = inject(HttpClient);
-    private api = 'iwonder-gzewb3d3hwafhma7.indiasouthcentral-01.azurewebsites.net/api/Forecast';
+    private api = 'https://iwonder-gzewb3d3hwafhma7.indiasouthcentral-01.azurewebsites.net/api/Forecast';
 
     getForecast(): Observable<Forecast[]> {
         return this.http.get<Forecast[]>(this.api);
     }
 
     getForecastByYear(year: number, panelName: string, productName: string, predictionType: 'M' | 'W'): Observable<Forecast[]> {
-        return this.http.get<Forecast[]>(`${this.api}/year/${year}?panelName=${panelName}&productName=${productName}&predictionType=${predictionType}`);
+        const params = new HttpParams()
+            .set('panelName', panelName)
+            .set('productName', productName)
+            .set('predictionType', predictionType);
+
+        return this.http.get<Forecast[]>(`${this.api}/year/${year}`, { params });
     }
 
     getFutureForecast(year1: number, year2: number, panelName: string, productName: string, predictionType: 'M' | 'W'): Observable<{
         year1: Forecast[];
         year2: Forecast[];
     }> {
+        const params = new HttpParams()
+            .set('year1', year1)
+            .set('year2', year2)
+            .set('panelName', panelName)
+            .set('productName', productName)
+            .set('predictionType', predictionType);
+
         return this.http.get<{
             year1: Forecast[];
             year2: Forecast[];
-        }>(`${this.api}/ForecastComparison?year1=${year1}&year2=${year2}&panelName=${panelName}&productName=${productName}&predictionType=${predictionType}`);
+        }>(`${this.api}/ForecastComparison`, { params });
     }
 
     // Get current year and last year
